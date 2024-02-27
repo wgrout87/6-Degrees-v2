112d865cffb9f10d0256f26115aa188626e68ecd
WGrout <wgrout87@gmail.com>
2024-02-26 17:07:12 -0700

    fixed typing issue in authrequired provider

--- src/app/components/AuthRequired.tsx
+++ src/app/components/AuthRequired.tsx
@@ -2,15 +2,17 @@ import { ReactNode, useEffect } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-function AuthRequired({ children }: { children: ReactNode }) {
+function AuthRequired({ children }: { children: ReactNode }): JSX.Element {
   const { user } = useAuthContext();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) navigate("/");
+    if (!user) navigate("/signup");
   }, [user, navigate]);
 
-  return user ? children : null;
+  return (<>
+  {user ? children : null}
+  </>)
 }
 
 export default AuthRequired;
