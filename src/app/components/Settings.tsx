72889d0a75a456f6241a8bdc76dc2575a49731b2
WGrout <wgrout87@gmail.com>
2024-03-14 17:57:55 -0600

    moved css styles to app level css

--- src/app/components/Settings.tsx
+++ src/app/components/Settings.tsx
@@ -1,12 +1,10 @@
-import "./styles.css"
-
 export default function NewGame() {
   const setTheme = (theme: string) => document.documentElement.className = theme;
   return (
     <div>
       <h1>Settings</h1>
       <h3>Color Scheme:</h3>
-      <button className="theme-btn" onClick={() => setTheme("theme_1")}>1</button>
+      <button className="theme-btn" onClick={() => setTheme("")}>1</button>
       <button className="theme-btn" onClick={() => setTheme("theme_2")}>2</button>
       <button className="theme-btn" onClick={() => setTheme("theme_3")}>3</button>
     </div>
